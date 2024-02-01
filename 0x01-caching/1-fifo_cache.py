108fbcf0c0091bca2f6a8df09532c8b94723c394
kumdan job <redeks123456@gmail.com>
2024-02-01 18:50:55 +0100

    cching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
-'''0x01. Caching'''
+"""0x01. Caching"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """FIFOCache implementation"""
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        i = list(self.cache_data.keys())
+        if len(i) > BaseCaching.MAX_ITEMS:
+            self.cache_data.pop(i[0])
+            print("DISCARD: {}".format(i[0]))
+
+    def get(self, key):
+        """retrieves a key from the cache dictionary"""
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,2 +1,30 @@
 #!/usr/bin/env python3
-'''0x01. Caching'''
+"""0x01. Caching"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """FIFOCache implementation"""
+
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                val, _ = self.cache_data.popitem()
+                print("DISCARD: {}".format(val))
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """retrieves a key from the cache dictionary"""
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
