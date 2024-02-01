ace2d47f43edf8367b792d4ea26f9fb30952424a
kumdan job <redeks123456@gmail.com>
2024-02-01 17:41:51 +0100

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""0x01. Caching"""
+
+"""Task 0: Basic dictionary
+"""
+
 
 from parent import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """inherit from BaseCaching class"""
+    """A class `BasicCache` that inherits from `BaseCaching`
+    and is a caching system
+    """
 
     def put(self, key, item):
-        """adds a key and value to the cache dictionary"""
-        if item is None or key is None:
-            return
-
-        self.cache_data[key] = item
+        """assign to the dictionary `self.cache_data` the
+        `item` value for the key `key`
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """retrieves a key from the cache dictionary"""
-        if key is None:
-            return None
+        """return the value in `self.cache_data` linked to `key`"""
 
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
