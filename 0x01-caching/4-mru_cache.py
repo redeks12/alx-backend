9db7595f68ad0dfabf67c2919d2ff4f1812f8c7b
kumdan job <redeks123456@gmail.com>
2024-02-01 19:30:24 +0100

    caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,2 +1,33 @@
 #!/usr/bin/env python3
-'''0x01. Caching'''
+"""0x01. Caching"""
+
+from collections import OrderedDict
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """LIFOCache implementation"""
+
+    def __init__(self):
+        """Constructor for LIFOCache implementation"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Put a key into the cache object"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                val, _ = self.cache_data.popitem()
+                print("DISCARD: {}".format(val))
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+    def get(self, key):
+        """retrieves a key from the cache dictionary"""
+        if key is None:
+            return None
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        return self.cache_data.get(key)
