429055c5a347feeb5ff8098ed410eaa58071a7cf
kumdan job <redeks123456@gmail.com>
2024-02-01 17:29:53 +0100

    cching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""0x01. Caching"""
+
+BaseCaching = __import__("parent").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """inherit from BaseCaching class"""
+
+    def put(self, key, item):
+        """adds a key and value to the cache dictionary"""
+        if item is None or key is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """retrieves a key from the cache dictionary"""
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,7 @@
+# 0x01. Caching
+Tasks
+### `0. Basic dictionary`
+### `1. FIFO caching`
+### `2. LIFO Caching`
+### `3. LRU Caching`
+### `4. MRU Caching`
