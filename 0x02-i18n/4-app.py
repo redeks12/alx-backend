2c1e3121119626ea780e772877f9ae72a3e3bfbe
kumdan job <redeks123456@gmail.com>
2024-02-06 17:38:11 +0100

    i18n

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,2 +1,39 @@
-#!/usr/bin/env python3
-'''0x02. i18n'''
+# !/usr/bin/env python3
+"""0x02. i18n"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Returns the locale of the application"""
+    loc = request.args.get("locale")
+    if loc and loc in app.config["LANGUAGES"]:
+        return loc
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", methods=["GET"])
+def home():
+    """default home page"""
+    return render_template(
+        "3-index.html",
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
